9c1939b29245e6c387133c06f7a9f66052837ec5
michal sladewski <michal.sladewski@gmail.com>
2021-07-26 18:50:34 +0200

    commit initAccordion fix

--- src/js/script.js
+++ src/js/script.js
@@ -111,34 +111,30 @@
     
     initAccordion() {
       const thisProduct = this;
-
       /* find the clickable trigger (the element that should react to clicking) */
       const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      console.log('clickableTrigger', clickableTrigger);
 
       /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function (event) {
-
+      clickableTrigger.addEventListener('click', function(event) {
         /* prevent default action for event */
         event.preventDefault();
 
         /* find active product (product that has active class) */
         const activeProduct = document.querySelectorAll(select.all.menuProductsActive);
-        console.log('activeProduct: ', activeProduct);
+        console.log('activeProduct', activeProduct);
 
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-        
+        for (let active of activeProduct) {
+          if (active !== thisProduct.element) {
 
-        /*for (let activeProduct of activeProducts) { */
-        if (activeProduct && activeProduct != thisProduct.element) {
-          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
-          console.log('remove class active');
+            active.classList.remove(classNames.menuProduct.wrapperActive);
+          }
         }
-        /* } else {
-          thisProduct.element.classList.add('active');
-        } */
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
+
     }
   }
   app.init();
